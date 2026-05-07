6b16d46527df22130109414fe7957ba4364cd368
adirathodd <88114611+adirathodd@users.noreply.github.com>
2026-05-06 20:06:08 -0400

    chore: update app startup to use environment port and disable debug mode

--- stage_3/app.py
+++ stage_3/app.py
@@ -472,7 +472,7 @@ def predict(n_clicks, input_value):
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 8050))
-    # app.run(debug=False, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=False, host="0.0.0.0", port=port)
 
-    app.run(debug=True)
+    # app.run(debug=True)
